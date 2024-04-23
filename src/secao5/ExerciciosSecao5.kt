2a0157f7b2a08bfd5b48141b6271250a88b4e9e5
Raul Ward <raulwarddev@gmail.com>
2024-04-23 13:18:57 -0300

    Creating repository and adding section 5

---
+++ src/secao5/ExerciciosSecao5.kt
@@ -0,0 +1,15 @@
+package secao5
+
+fun main() {
+
+    var name: String = "Raul Ward de Araujo"
+    var text: String
+    val size: Short = 40
+    var pib: Long = 1869000000000
+    var population: Int = 211_000_000
+
+    var pibPerCapita: Long = pib / population
+
+    println("O PIB per capita é: $pibPerCapita")
+
+}
---
+++ src/secao5/FormatacaoStrings.kt
@@ -0,0 +1,13 @@
+package secao5
+
+fun main() {
+    var nome: String = "Raul"
+    val nome2: String = "Giulia"
+
+    var sobrenome: String = "Ward"
+    val sobrenome2: String = "Vieira"
+
+    println("Hello $nome $sobrenome")
+    println("Hello $nome2 $sobrenome2")
+
+}
