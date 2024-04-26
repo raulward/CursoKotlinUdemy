91803a63462d93ea932568dce23718bd043aa7fc
Raul Ward <raulwarddev@gmail.com>
2024-04-26 11:59:07 -0300

    adding section 12

---
+++ src/secao12/AnyNothingUnit.kt
@@ -0,0 +1,18 @@
+package secao12
+
+fun main() {
+    values(true)
+    values(10)
+}
+
+fun values(value: Any) {
+
+}
+
+fun fooWithoutReturn(): Unit {
+
+}
+
+fun nothingFoo(): Nothing {
+    TODO("Not implemented")
+}
